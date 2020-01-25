1a621302dcbca78e6009cbc2bb0e7fc7b8d64a95
SebastianGarces <gsgarces1@gmail.com>
2020-01-25 00:01:17 -0500

    completed first STRETCH - added carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,63 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector(".carousel-container");
+const photoPaths = [
+	"./assets/carousel/mountains.jpeg",
+	"./assets/carousel/computer.jpeg",
+	"./assets/carousel/trees.jpeg",
+	"./assets/carousel/turntable.jpeg"
+];
+
+const Carousel = () => {
+	const carousel = document.createElement("div");
+	carousel.classList.add("carousel");
+
+	const leftBtn = document.createElement("div");
+	leftBtn.classList.add("left-button");
+	carousel.appendChild(leftBtn);
+
+	const rightBtn = document.createElement("div");
+	rightBtn.classList.add("right-button");
+	carousel.appendChild(rightBtn);
+
+	const photos = photoPaths.map(photo => {
+		const carouselImg = document.createElement("img");
+		carouselImg.src = photo;
+		carousel.appendChild(carouselImg);
+		return carouselImg;
+	});
+
+	let photoIndex = 1;
+
+	const showPhoto = index => {
+		index > photos.length ? (photoIndex = 1) : index;
+		index < 1 ? (photoIndex = photos.length) : index;
+		console.log(index, photoIndex);
+
+		photos.forEach(photo => (photo.style.display = "none"));
+
+		photos[photoIndex - 1].style.display = "block";
+	};
+
+	showPhoto(photoIndex);
+
+	const plusIndex = num => {
+		showPhoto((photoIndex += num));
+	};
+
+	leftBtn.addEventListener("click", () => {
+		plusIndex(-1);
+	});
+
+	rightBtn.addEventListener("click", () => {
+		plusIndex(1);
+	});
+
+	console.log(photos);
+
+	return carousel;
+};
+
+carouselContainer.appendChild(Carousel());
