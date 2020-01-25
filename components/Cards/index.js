422529455a8f7770327b3196c22d6fb30be613ad
SebastianGarces <gsgarces1@gmail.com>
2020-01-24 22:16:43 -0500

    completed MVP - tabs and cards components

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const Card = article => {
+	const card = document.createElement("div");
+	card.classList.add("card");
+
+	const headline = document.createElement("div");
+	headline.classList.add("headline");
+	headline.textContent = article.headline;
+	card.appendChild(headline);
+
+	const author = document.createElement("div");
+	author.classList.add("author");
+	card.appendChild(author);
+
+	const imgContainer = document.createElement("div");
+	imgContainer.classList.add("img-container");
+	author.appendChild(imgContainer);
+
+	const img = document.createElement("img");
+	img.src = article.authorPhoto;
+	imgContainer.appendChild(img);
+
+	const authorNameSpan = document.createElement("span");
+	authorNameSpan.textContent = `By ${article.authorName}`;
+	author.appendChild(authorNameSpan);
+
+	return card;
+};
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios
+	.get("https://lambda-times-backend.herokuapp.com/articles")
+	.then(response => {
+		Object.values(response.data.articles).forEach(topic => {
+			topic.forEach(article => {
+				const newArticle = Card(article);
+				cardContainer.appendChild(newArticle);
+			});
+		});
+	})
+	.catch(err => console.log("error: ", err));
